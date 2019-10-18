8c83d1104ced1950cb79281765950178789506fe
sohelrkhan <55046835+sohelrkhan@users.noreply.github.com>
2019-10-18 15:42:00 +0600

    Salary App

---
+++ SalaryCalculatorApp/SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+        SalaryCount _salaryCount = new SalaryCount();
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            string msg;
+            _salaryCount.employeename = employeeNameTextBox.Text;
+            _salaryCount.basicsalary = Convert.ToDouble(basicAmountTextBox.Text);
+            _salaryCount.houserent = Convert.ToDouble(homeRentTextBox.Text);
+            _salaryCount.medicalallowance = Convert.ToDouble(medicalAllowanceTextBox.Text);
+            msg = _salaryCount.showmesalary();
+            MessageBox.Show(msg);
+
+        }
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculatorAppPractice3/SalaryCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class SalaryCount
+    {
+        public string employeename;
+        public double basicsalary;
+        public double salary;
+        public double houserent;
+        public double medicalallowance;
+        public double totalsalary()
+        {
+            houserent = (basicsalary * houserent) / 100;
+            medicalallowance = (basicsalary * medicalallowance) / 100;
+            salary = (basicsalary +houserent+medicalallowance );
+            return salary;
+        }
+        public string showmesalary()
+        {
+            var msg = employeename +" "+ "your salary is:" + totalsalary() ;
+            return msg;
+        }
+      
+    }
+}
